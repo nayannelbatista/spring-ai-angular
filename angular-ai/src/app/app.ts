3e0d586a9f7406059a87d2486c4faaadafceb04e
Nayanne Batista <nayannelopes.bd@gmail.com>
2026-01-19 16:00:36 -0300

    feat: add Angular Material and basic toolbar

--- angular-ai/src/app/app.ts
+++ angular-ai/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MatButtonModule, MatToolbarModule, RouterLink, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-ai');
+  protected readonly title = signal('AI-Spring-Angular');
 }
